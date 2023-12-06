da665649842772ac4441ee70626c6e6bcfdb4b1f
NeuHezxp <mpatino@student.neumont.edu>
2023-12-06 10:25:08 -0700

    added triangle render

--- Source/RayTracer/Main.cpp
+++ Source/RayTracer/Main.cpp
@@ -11,6 +11,8 @@
 #include "Material.h"
 #include <ctime>
 
+#include "Triangle.h"
+
 int main(int, char**) {
     std::cout << "Hello World" << std::endl;
     ray::seedRandom(static_cast<unsigned int>(time(nullptr)));
@@ -27,15 +29,15 @@ int main(int, char**) {
 
     ray::Scene scene(8, glm::vec3{ 1.0f }, glm::vec3{ 0.5f, 0.7f, 1.0f });
     scene.SetCamera(camera);
-
-    for (int x = -10; x < 10; x++)
+    /*
+    for (int x = -5; x < 5; x++)  // Adjust the x loop range
     {
-        for (int z = -10; z < 10; z++)
+        for (int z = -5; z < 5; z++)  // Adjust the z loop range
         {
 
             std::shared_ptr<ray::Material> material;
 
-            // create random material
+            // create random material   
             float r = ray::random01(); 
             if (r < 0.3f)		material = std::make_shared<ray::Lambertian>(glm::rgbColor(glm::vec3{ ray::random(0, 360), 1.0f, 1.0f })); 
             else if (r < 0.6f)	material = std::make_shared<ray::Metal>(ray::color3_t{ ray::random(0.5f, 1.0f) }, ray::random(0, 0.5f)); 
@@ -49,7 +51,11 @@ int main(int, char**) {
             // add sphere to the scene
             scene.AddObject(std::move(sphere));
         }
-    }
+    }*/
+    auto triangleMaterial = std::make_shared<ray::Lambertian>(ray::color3_t{ 1,0,0 });
+    // Define three distinct vertices for the triangle
+    auto triangle = std::make_unique<ray::Triangle>(glm::vec3{ 0, 0, 0 }, glm::vec3{ 1, 0, 0 }, glm::vec3{ 0, 1, 0 }, triangleMaterial);
+    scene.AddObject(std::move(triangle));
 
     auto planeMaterial = std::make_shared<ray::Lambertian>(ray::color3_t{ 0.2f }); 
     auto plane = std::make_unique<ray::Plane>(glm::vec3{ 0, -0.3, 0 }, glm::vec3{ 0, 1, 0 }, planeMaterial);
